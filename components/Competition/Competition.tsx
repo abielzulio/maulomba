091937f924f4a516287e3ac6c90afe7bb868ec25
Abiel Zulio M <abielzm@gmail.com>
2022-10-23 17:19:33 +0700

    feat: competition: init

---
+++ components/Competition/Competition.tsx
@@ -0,0 +1,10 @@
+import { ContentContainer } from "components/Container"
+import Filter from "components/Filter"
+
+const CompetitionSection = () => (
+  <ContentContainer>
+    <Filter />
+  </ContentContainer>
+)
+
+export default CompetitionSection
