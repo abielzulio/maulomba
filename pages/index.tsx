257ad43ecf00009dc786e9f88d37c1d36f8365f7
Abiel Zulio M <abielzm@gmail.com>
2022-10-23 17:20:32 +0700

    feat: wip index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,40 @@
-import Author from "components/Author"
-import Editor from "components/Editor"
+import {
+  ArrowSmallDownIcon,
+  ArrowUpTrayIcon,
+} from "@heroicons/react/24/outline"
+import Button from "components/Button"
+import CompetitionSection from "components/Competition"
+import { ButtonContainer, HeroContainer, Page } from "components/Container"
 import Hero from "components/Hero"
+import useScrollPosition from "hooks/useScrollPosition"
 import type { NextPage } from "next"
 
 const Home: NextPage = () => {
+  const scrollPosition = useScrollPosition()
   return (
-    <section className="min-w-screen min-h-screen py-[30px] text-center flex flex-col align-center justify-center relative"></section>
+    <Page>
+      <HeroContainer style={{ filter: `blur(${scrollPosition / 25}px)` }}>
+        <Hero
+          title={["Cari kompetisi", "tanpa distraksi"]}
+          desc={[
+            "Cari, simpan, dan ikuti berbagai jenis kompetisi",
+            "dengan mudah dan nyaman",
+          ]}
+        />
+        <ButtonContainer>
+          <Button
+            icon={<ArrowUpTrayIcon className="h-[18px] w-[18px]" />}
+            title="Unggah Lomba"
+          />
+          <Button
+            icon={<ArrowSmallDownIcon className="h-[18px] w-[18px]" />}
+            title="Cari Lomba"
+            kind="secondary"
+          />
+        </ButtonContainer>
+      </HeroContainer>
+      <CompetitionSection />
+    </Page>
   )
 }
 
