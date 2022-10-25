8ef5a63b7d61b97baa4598686fcb28defbe65bcd
Abiel Zulio M <abielzm@gmail.com>
2022-10-25 22:43:16 +0700

    chore: add mantine ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,14 @@
 import Head from "components/Head"
 import type { AppProps } from "next/app"
 import "../styles/globals.css"
+import { MantineProvider } from "@mantine/core"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <MantineProvider>
       <Head />
       <Component {...pageProps} />
-    </>
+    </MantineProvider>
   )
 }
 
