e13d721fa5baa2b8b94f08f55ea9c787fe728009
Abiel Zulio M <abielzm@gmail.com>
2022-10-27 22:09:47 +0700

    fix: add more string

--- data/string.ts
+++ data/string.ts
@@ -3,3 +3,8 @@ export const STRING_FEATUED_PILL: string = "Featured"
 export const STRING_CATEGORY_TOGGLE_BUTTON: string = "Kategori"
 
 export const STRING_SEARCH_FILTER_PLACEHOLDER: string = "Cari lomba..."
+
+export const STRING_FILTERED_COMPETITON_IS_NOT_FOUND: string =
+  "Lomba yang Anda cari tidak ada"
+
+export const STRING_CLEAR_FILTERED_COMPETITON_BUTTON: string = "Hapus pencarian"
