feb2039b583eda62e1a033d616f9931f4f3acc58
Abiel Zulio M <abielzm@gmail.com>
2022-10-28 21:03:07 +0700

    refactor: hero

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,21 +1,31 @@
 interface HeroProps extends React.HTMLAttributes<HTMLDivElement> {
   hero_title: string[]
-  hero_desc: string[]
+  hero_desc?: string[]
 }
 
 const Hero = (props: HeroProps) => {
   const { hero_title, hero_desc, ...rest } = props
   return (
-    <div className="flex flex-col gap-[20px]" {...rest}>
+    <div
+      className={`flex flex-col ${hero_desc ? `gap-[20px]` : `gap-[0px]`}`}
+      {...rest}
+    >
       <h1 className="text-[36px] font-semibold tracking-tight md:text-[54px]">
-        {hero_title[0]}, <br />
-        <span className="mr-auto bg-gradient-to-l from-blue-500 to-white bg-clip-text text-transparent">
-          {hero_title[1]}
-        </span>
+        {hero_title[0] && hero_title[0]}
+        {hero_title[1] && (
+          <>
+            ,<br />
+            <span className="mr-auto bg-gradient-to-l from-blue-500 to-white bg-clip-text text-transparent">
+              {hero_title[1]}
+            </span>
+          </>
+        )}
       </h1>
-      <p className="text-[14px] font-normal text-gray-50 text-opacity-80 md:text-[18px]">
-        {hero_desc[0]} <br /> {hero_desc[1]}
-      </p>
+      {hero_desc && (
+        <p className="text-[14px] font-normal text-gray-50 text-opacity-80 md:text-[18px]">
+          {hero_desc[0]} <br /> {hero_desc[1]}
+        </p>
+      )}
     </div>
   )
 }
