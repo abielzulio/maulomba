eac4b4bf3b6b9714fd53a325a7dc343bace4559a
Abiel Zulio M <abielzm@gmail.com>
2022-11-14 18:59:36 +0700

    fix: supabase env key undefined

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js"
 
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY
+  process.env.NEXT_PUBLIC_SUPABASE_URL?.toString() || "",
+  process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY?.toString() || ""
 )
 
 export const SUPABASE_BUCKET_BASE_URL = `https://${process.env.NEXT_PUBLIC_SUPABASE_PROJECT_ID}.supabase.in/storage/v1/object/public`
