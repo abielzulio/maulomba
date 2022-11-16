d36b698f37c28cdbc009c3a381afbdde0ad81bea
Abiel Zulio M <abielzm@gmail.com>
2022-11-17 06:41:16 +0700

    refactor: index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,49 +1,42 @@
-import { ArrowUpTrayIcon, UserIcon } from "@heroicons/react/24/outline"
+import { ArrowUpTrayIcon } from "@heroicons/react/24/outline"
 import Button from "components/Button"
 import { CompetitionSection } from "components/Competition"
 import { CTAContainer, HeroContainer, Page } from "components/Container"
+import Head from "components/Head"
 import Hero from "components/Hero"
-import useScrollPosition from "hooks/useScrollPosition"
 import type { NextPage } from "next"
 import NextLink from "next/link"
-import { useWindowDimension } from "hooks/useWindowDimension"
 
 const Home: NextPage = () => {
-  const scrollPosition = useScrollPosition()
-  const isMobile = useWindowDimension() < 640
   return (
-    <Page>
-      <HeroContainer>
-        <Hero
-          hero_title={["Cari kompetisi", "tanpa distraksi"]}
-          hero_desc={[
-            "Cari, simpan, dan ikuti berbagai jenis kompetisi",
-            "dengan mudah dan nyaman",
-          ]}
-        />
-        <CTAContainer>
-          <NextLink href="/kirim-lomba">
-            <Button
-              icon={<ArrowUpTrayIcon className="h-4 w-4" />}
-              title={"Unggah lomba"}
-              label="Gratis!"
-              kind="primary"
-              size={"medium"}
-              width="full"
-              className="sticky top-[20px] mr-auto"
-            />
-          </NextLink>
-          {/*           <NextLink href="/masuk">
-            <Button
-              icon={<UserIcon className="h-4 w-4" />}
-              kind="secondary"
-              className="sticky top-[20px]"
-            />
-          </NextLink> */}
-        </CTAContainer>
-      </HeroContainer>
-      <CompetitionSection />
-    </Page>
+    <>
+      <Head />
+      <Page>
+        <HeroContainer>
+          <Hero
+            hero_title={["Cari kompetisi", "tanpa distraksi"]}
+            hero_desc={[
+              "Cari, simpan, dan ikuti berbagai jenis kompetisi",
+              "dengan mudah dan nyaman",
+            ]}
+          />
+          <CTAContainer>
+            <NextLink href="/kirim-lomba">
+              <Button
+                icon={<ArrowUpTrayIcon className="h-4 w-4" />}
+                title={"Unggah lomba"}
+                label="Gratis!"
+                kind="primary"
+                size={"medium"}
+                width="full"
+                className="sticky top-[20px] mr-auto"
+              />
+            </NextLink>
+          </CTAContainer>
+        </HeroContainer>
+        <CompetitionSection />
+      </Page>
+    </>
   )
 }
 
