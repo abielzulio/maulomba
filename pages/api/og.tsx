d6d9cf0423cf1428bd61bc1dee5163cd6bab234d
Abiel Zulio M <abielzm@gmail.com>
2022-11-19 17:17:21 +0700

    api: add error handler + change url param method

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -6,68 +6,69 @@ export const config = {
 }
 
 export default async function handler(req: NextRequest) {
-  const { searchParams } = req.nextUrl
-  const title = searchParams.get("title")
-  const eo = searchParams.get("eo")
-  /*   const tags = searchParams.get("tags") */
-  const img = searchParams.get("img")
+  try {
+    const { searchParams } = new URL(req.url)
+    const title = searchParams.get("title")
+    const eo = searchParams.get("eo")
+    /*   const tags = searchParams.get("tags") */
+    const img = searchParams.get("img")
 
-  if (!title || !eo) {
+    if (!title || !eo) {
+      return new ImageResponse(
+        (
+          <img
+            width="1200"
+            height="630"
+            // change to actual domain
+            src="https://mau-lomba.vercel.app/og.png"
+            style={{ objectFit: "cover", height: "100%", width: "100%" }}
+          />
+        ),
+        {
+          width: 1200,
+          height: 630,
+        }
+      )
+    }
+
     return new ImageResponse(
       (
-        <img
-          width="1200"
-          height="630"
-          // change to actual domain
-          src="https://mau-lomba.vercel.app/og.png"
-          style={{ objectFit: "cover", height: "100%", width: "100%" }}
-        />
-      ),
-      {
-        width: 1200,
-        height: 630,
-      }
-    )
-  }
-
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          display: "flex",
-          color: "#f0f6fc",
-          backgroundImage:
-            "linear-gradient(to bottom right, #0d1116 75%, #0a2442)",
-          width: "100%",
-          height: "100%",
-          gap: 40,
-          flexDirection: "row",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <img
-          width="600"
-          height="630"
-          src="https://mau-lomba.vercel.app/1.jpg"
-          style={{ objectFit: "cover", height: "100%", width: "100%" }}
-        />
         <div
           style={{
             display: "flex",
-            flexDirection: "column",
-            justifyContent: "left",
-            alignItems: "left",
+            color: "#f0f6fc",
+            backgroundImage:
+              "linear-gradient(to bottom right, #0d1116 75%, #0a2442)",
+            width: "100%",
+            height: "100%",
             gap: 40,
-            paddingTop: 30,
-            paddingBottom: 30,
-            paddingLeft: 20,
-            paddingRight: 20,
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
           }}
         >
-          <h1 style={{ fontSize: 45, fontWeight: 600 }}>{title}</h1>
-          <p style={{ fontSize: 35, opacity: 0.5 }}>{eo}</p>
-          {/*           <div
+          <img
+            width="600"
+            height="630"
+            src="https://mau-lomba.vercel.app/1.jpg"
+            style={{ objectFit: "cover", height: "100%", width: "100%" }}
+          />
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "left",
+              alignItems: "left",
+              gap: 40,
+              paddingTop: 30,
+              paddingBottom: 30,
+              paddingLeft: 20,
+              paddingRight: 20,
+            }}
+          >
+            <h1 style={{ fontSize: 45, fontWeight: 600 }}>{title}</h1>
+            <p style={{ fontSize: 35, opacity: 0.5 }}>{eo}</p>
+            {/*           <div
             style={{
               display: "flex",
               flexDirection: "row",
@@ -93,12 +94,18 @@ export default async function handler(req: NextRequest) {
                 </p>
               ))}
           </div> */}
+          </div>
         </div>
-      </div>
-    ),
-    {
-      width: 1200,
-      height: 630,
-    }
-  )
+      ),
+      {
+        width: 1200,
+        height: 630,
+      }
+    )
+  } catch (e: any) {
+    console.log(`${e.message}`)
+    return new Response(`Failed to generate the image`, {
+      status: 500,
+    })
+  }
 }
