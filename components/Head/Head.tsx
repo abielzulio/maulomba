d878bbed0f6c17d5bfe4bea5041fa17a2c76358d
Abiel Zulio M <abielzm@gmail.com>
2022-11-21 12:15:15 +0700

    refactor: head

--- components/Head/Head.tsx
+++ components/Head/Head.tsx
@@ -12,33 +12,32 @@ interface HeadProps {
 }
 
 const Head = (props: HeadProps) => {
+  const {
+    title = product.name + " | " + product.description.short,
+    description = product.description.long,
+    keywords = product.keywords,
+    img = product.url + product.img,
+    url = product.url,
+    author = product.author,
+    favicon = product.url + product.favicon,
+  } = props
   return (
     <NextHead>
-      <link rel="shortcut icon" href={props.favicon} />
-      <title>{props.title}</title>
+      <link rel="shortcut icon" href={favicon} />
+      <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <meta name="description" content={props.description} />
-      <meta name="keywords" content={props.keywords} />
-      <meta name="author" content={props.author} />
-      <meta property="og:image" content={props.img} />
-      <meta name="og:description" content={props.description} />
-      <meta property="og:title" content={props.title} />
-      <meta property="og:url" content={props.url} />
-      <meta property="og:site_name" content={props.title} />
+      <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
+      <meta name="author" content={author} />
+      <meta property="og:image" content={img} />
+      <meta name="og:description" content={description} />
+      <meta property="og:title" content={title} />
+      <meta property="og:url" content={url} />
+      <meta property="og:site_name" content={title} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     </NextHead>
   )
-}
-
-Head.defaultProps = {
-  title: product.name + " | " + product.description.short,
-  description: product.description.long,
-  keywords: product.keywords,
-  img: product.url + product.img,
-  url: product.url,
-  author: product.author,
-  favicon: product.url + product.favicon,
 }
 
 export default Head
