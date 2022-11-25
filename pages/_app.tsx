6bf7c831603681e9fa2dd57b2704ccf6ef723d55
Abiel Zulio M <abielzm@gmail.com>
2022-11-25 17:04:20 +0700

    feat: add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,48 @@ import type { AppProps } from "next/app"
 import "../styles/globals.css"
 import { MantineProvider } from "@mantine/core"
 import Footer from "components/Footer"
+import Script from "next/script"
+import { useRouter } from "next/router"
+import { useEffect } from "react"
+import * as gtag from "lib/gtag"
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url)
+    }
+    router.events.on("routeChangeComplete", handleRouteChange)
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange)
+    }
+  }, [router.events])
   return (
-    <MantineProvider>
-      <Component {...pageProps} />
-      <Footer />
-    </MantineProvider>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-94DZGE8HM1"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-94DZGE8HM1', {
+page_path: window.location.pathname,
+});
+`,
+        }}
+      />
+      <MantineProvider>
+        <Component {...pageProps} />
+        <Footer />
+      </MantineProvider>
+    </>
   )
 }
 
